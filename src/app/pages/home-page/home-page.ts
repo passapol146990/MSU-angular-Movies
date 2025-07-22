6c03438c5f4ae016e9f75ef5729b866f48988337
Passapol <phasphlsuthahthrrm976@gmail.com>
2025-07-22 08:33:41 +0700

    x

---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Headers } from "../../components/headers/headers";
+
+@Component({
+  selector: 'app-home-page',
+  imports: [Headers],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.scss'
+})
+export class HomePage {
+
+}
